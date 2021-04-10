796d96be6227cd234fe5dc1e5a12239743604c21
Danny Serrano <danser829@gmail.com>
2021-04-10 17:06:45 -0400

    FE: create loading screen

--- static/index.js
+++ static/index.js
@@ -1,18 +1,21 @@
 
 const tiles = document.getElementById('container')
 const list = document.createElement('ul');
+const header = document.getElementById('header')
+const loader = document.getElementById('loader')
+const nav = document.getElementById('nav')
 
 // popular anime (in my opinion)
-const popularAnime = ['Naruto', 'One Piece', 'Death Note']
+const popularAnime = ['Naruto', 'One Piece', 'Death Note', 'Sword Art Online', 'Fairy Tale', 'My Hero Academia', 'Haikyuu!!', 'Hunter x Hunter', 'Cowboy Bepop', 'One Punch Man', 'Sailor Moon', 'Dragon Ball', 'Soul Eater', 'Yuri!!! on Ice', 'Bleach']
 // array of tile colors
 const colors = ['cyan', 'purple', 'pink', 'blue']
 
 // create list of anime tiles
 function animeList(arr) {
-  arr.forEach(anime => {
+  arr.forEach((anime, idx) => {
     const li = document.createElement('li')
     li.textContent = anime;
-    li.classList = `${colors[Math.floor(Math.random() * 4)]} tile`
+    li.classList = `${colors[idx % 4]} tile`
     list.appendChild(li);
   })
   tiles.appendChild(list)
@@ -20,29 +23,39 @@ function animeList(arr) {
 
 
 async function getRecs(anime) {
-  console.log('selected', anime)
+  // activate loading screen
+  tiles.style.display = "none";
+  nav.style.display = 'none';
+  loader.classList = 'loading'
+  // request recommendations for selected anime
   const res = await fetch(`/recommendations/${anime}`, {
     method: 'GET',
     headers: {
       'Access-Control-Allow-Origin': '*'
     }
   })
-  console.log('request sent', res)
   const recs = await res.json()
-  console.log('recs', recs.recs)
-
+  const recsArray = recs.recs
+  // deactivate loading screen
+  tiles.style.display = "inline";
+  nav.style.display = "inline";
+  loader.classList = 'loaded'
+  header.innerText = `People who watched ${recsArray.shift()} also enjoyed:`
+  // remove current anime tiles
   while(list.firstChild) {
     list.removeChild(list.firstChild)
   }
-  animeList(recs.recs)
+  // replace tiles with recommenation anime tiles
+  animeList(recsArray)
 }
 
 tiles.addEventListener('click', (event) => {
   const tile = event.target
   if (tile.classList.contains('tile')) {
-    console.log('innertext', tile.innerText)
     getRecs(tile.innerText)
   }
 })
+
+list.classList = 'flex'
 
 animeList(popularAnime)
